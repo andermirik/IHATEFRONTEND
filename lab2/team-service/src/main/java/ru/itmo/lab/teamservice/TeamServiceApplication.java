eb0150641d28d415ee8fce8058076bfe6f502c73
andermirik <andermirik@gmail.com>
2022-06-17 04:28:18 +0300

    config server

--- lab2/team-service/src/main/java/ru/itmo/lab/teamservice/TeamServiceApplication.java
+++ lab2/team-service/src/main/java/ru/itmo/lab/teamservice/TeamServiceApplication.java
@@ -2,11 +2,13 @@ package ru.itmo.lab.teamservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.openfeign.EnableFeignClients;
 import org.springframework.transaction.annotation.EnableTransactionManagement;
 
 @EnableTransactionManagement
 @EnableFeignClients
+@EnableDiscoveryClient
 @SpringBootApplication
 public class TeamServiceApplication {
 
